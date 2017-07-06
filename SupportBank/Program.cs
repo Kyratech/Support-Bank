0dd321847c7ad7dfef70efdbd451201d9fac79c4
Matthew Langford <Mathew.Langford@softwire.com>
2017-07-06 17:13:12 +0100

    Added support for logging.

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -1,17 +1,38 @@
 ﻿using System;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace SupportBank
 {
     class Program
     {
+        private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
-            string transactionPath = "C:/Work/Training/SupportBank/Transactions/Transactions2014.csv";
+            Program.SetupLogger();
+            logger.Info("SupportBank program started");
+
+            string transactions2014 = "C:/Work/Training/SupportBank/Transactions/Transactions2014.csv";
+            string transactions2015 = "C:/Work/Training/SupportBank/Transactions/DodgyTransactions2015.csv";
             SupportBank bank = new SupportBank();
-            bank.UpdateRecords(transactionPath);
+            bank.UpdateRecords(transactions2014);
+            //bank.UpdateRecords(transactions2015);
 
             SupportBankConsoleParser consoleParser = new SupportBankConsoleParser();
             consoleParser.ProcessInput(bank);
+
+            logger.Info("SupportBank program shutting down.");
+        }
+
+        private static void SetupLogger()
+        {
+            var config = new LoggingConfiguration();
+            var target = new FileTarget { FileName = @"C:\Work\Logs\SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}" };
+            config.AddTarget("File Logger", target);
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
+            LogManager.Configuration = config;
         }
     }
 }
